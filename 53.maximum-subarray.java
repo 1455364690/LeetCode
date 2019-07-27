b7e5dc8d0539aa93ddface860ef18af2d8cac469
sunjh <1455364690@qq.com>
2019-07-27 13:02:50 +0800

    53

--- 53.maximum-subarray.java
+++ 53.maximum-subarray.java
@@ -1,11 +1,5 @@
-
-public class Main {
-    public static void main(String[] args) {
-        int[] a = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
-        System.out.println(maxSubArray(a));
-    }
-
-    public static int maxSubArray(int[] nums) {
+class Solution {
+    public int maxSubArray(int[] nums) {
         if (nums.length == 1) {
             return nums[0];
         }
